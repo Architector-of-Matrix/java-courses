fe35440d2d060617ef968732f3c33c00ac0a39e3
Architector_of_matrix <architectorofmatrix2.0@gmail.com>
2016-01-04 21:55:01 +0100

    add new functional

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,27 @@ public class Calculator {
 
     private int result;
 
-    public void add(int... params) {
-        for (Integer param : params) {
-            this.result += param;
-        }
+    public void add(int first, int second) {
+        this.result = first + second;
     }
+
+    public void extract(int first, int second) {
+        this.result = first - second;
+
+    }
+
+    public void multiply(int first, int second) {
+        this.result = first * second; 
+    }
+
+    public void devide(int first, int second) {
+        this.result = (int) first / second;
+    }
+
+    public void power(int first, int second) {
+        double powResult = Math.pow(Double.valueOf(first), Double.valueOf(second));
+        this.result = (int) powResult; 
+    }       
 
     public int getResult() {
         return this.result;
--- InteractRunner.java
+++ InteractRunner.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class InteractRunner {
 
+    private static boolean clean = true;
+
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
@@ -9,16 +11,65 @@ public class InteractRunner {
             Calculator calc = new Calculator();
             String exit = "no";
 
-            while (!exit.equals("yes")) {
-                System.out.println("Enter first arg: ");
-                String first = reader.next();
+            while (true) {
+                System.out.println("Please select needed action: ");
+                System.out.println("1 - Sum");
+                System.out.println("2 - Extract");
+                System.out.println("3 - Multiply");
+                System.out.println("4 - Devide");
+                int action = Integer.valueOf(reader.next());
+                String first;
+
+                if(clean){
+                    System.out.println("Enter first arg: ");
+                    first = reader.next();
+                } else {
+                    System.out.println("First arg: ");
+                    System.out.println(calc.getResult());
+                    Integer firstInt = (Integer) calc.getResult();
+                    first = firstInt.toString();
+                }
+
                 System.out.println("Enter second arg: ");
                 String second = reader.next();
-                calc.add(Integer.valueOf(first), Integer.valueOf(second));
+
+                switch (action) {
+                    case 1:
+                        //sum
+                        calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                    case 2:
+                        //Extract
+                        calc.extract(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+
+                    case 3:
+                        //Multiply
+                        calc.multiply(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                    case 4:
+                        //Devide
+                        calc.devide(Integer.valueOf(first), Integer.valueOf(second));
+                        break;
+                }
+
                 System.out.println("Result: " + calc.getResult());
-                calc.cleanResult();
+
+                
                 System.out.println("Exit : yes/no");
                 exit = reader.next();
+                if (exit.equals("yes")) {
+                    break;
+                }
+
+                System.out.println("Clear : yes/no");
+
+                if (reader.next().equals("yes")) {
+                    calc.cleanResult();
+                    clean = true;
+                } else {
+                    clean = false;
+                }
             }
 
         } finally {
