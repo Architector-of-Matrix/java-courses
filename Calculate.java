1915afb8297f3aa3adff4a534e6c89115647ae80
Architector_of_matrix <architectorofmatrix2.0@gmail.com>
2016-01-04 21:06:33 +0100

    user input

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,16 @@
 public class Calculate {
 
     public static void main(String[] args) {
-        System.out.println("calculate");
+        System.out.println("calculate...");
+        
+        double first = Double.valueOf(args[0]);
+        double second = Double.valueOf(args[1]);
+        double summ = first + second;
+        double pow = Math.pow(first, second);
+
+        System.out.println("Sum: " + summ);
+        System.out.println("Power: " + pow);
+
     }
 
 }
